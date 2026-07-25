738504ed958501600c5790b419361c1c39255406
psdwizzard <k.loebenberg@gmail.com>
2026-07-25 13:15:36 -0400

    match GEDE01 game function at 80008D0C

---
+++ src/game/game_fn_80008D0C.c
@@ -0,0 +1,24 @@
+typedef signed char s8;
+typedef int s32;
+
+typedef struct EventState {
+    s32 index;
+    unsigned char* records;
+    s32 frame;
+    s32 remaining;
+    s32 count;
+    void* handle;
+    s8 mode;
+    s8 active;
+    s8 enabled;
+    s8 pad_1B;
+    s32 actor;
+} EventState;
+
+extern EventState lbl_80302040;
+
+void fn_80008D0C(void)
+{
+    lbl_80302040.index = -1;
+    lbl_80302040.frame = 0;
+}
