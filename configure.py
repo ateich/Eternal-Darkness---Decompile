2be2410ecf6456715e148b4f6534413ab140f556
psdwizzard <k.loebenberg@gmail.com>
2026-07-25 14:33:08 -0400

    Match fn_800099D8

--- configure.py
+++ configure.py
@@ -113,6 +113,16 @@ config.custom_build_rules = [
             "&& touch $out"
         ),
         "description": "EXTERNALIZE $in",
+    },
+    {
+        "name": "externalize_game_bias_12",
+        "command": (
+            "python3 tools/externalize_elf_symbol.py $in @12 && "
+            "build/binutils/powerpc-eabi-objcopy "
+            "--redefine-sym=@12=lbl_8064DCE8 --remove-section=.sdata2 $in "
+            "&& touch $out"
+        ),
+        "description": "EXTERNALIZE $in",
     }
 ]
 config.custom_build_steps = {
@@ -131,6 +141,11 @@ config.custom_build_steps = {
             "outputs": [f"build/{VERSION}/src/game/game_fn_800098C0.externalized"],
             "rule": "externalize_game_bias_17",
             "inputs": [f"build/{VERSION}/src/game/game_fn_800098C0.o"],
+        },
+        {
+            "outputs": [f"build/{VERSION}/src/game/game_fn_800099D8.externalized"],
+            "rule": "externalize_game_bias_12",
+            "inputs": [f"build/{VERSION}/src/game/game_fn_800099D8.o"],
         }
     ]
 }
@@ -223,6 +238,7 @@ config.libs = [
             Object(Matching, "game/game_fn_800094C0.c"),
             Object(Matching, "game/game_fn_800096E0.c"),
             Object(Matching, "game/game_fn_800098C0.c"),
+            Object(Matching, "game/game_fn_800099D8.c"),
             Object(Matching, "game/game_fn_8017A574.c"),
         ],
     },
