0b9e73af8e35b54bc538072b238e9f5e013ff31a
psdwizzard <k.loebenberg@gmail.com>
2026-07-25 13:13:56 -0400

    match GEDE01 game function at 80008C14

--- configure.py
+++ configure.py
@@ -153,6 +153,7 @@ config.libs = [
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
+            Object(Matching, "game/game_fn_80008C14.c"),
             Object(Matching, "game/game_fn_8017A574.c"),
         ],
     },
