bc76ba787a1cc8033a4998504ca7f65e0904f0d5
psdwizzard <k.loebenberg@gmail.com>
2026-07-28 08:47:51 -0400

    Match fn_8000C400

--- configure.py
+++ configure.py
@@ -292,6 +292,11 @@ config.custom_build_steps = {
             "rule": "externalize_game_bias_9",
             "inputs": [f"build/{VERSION}/src/game/game_fn_8000C17C.o"],
         },
+        {
+            "outputs": [f"build/{VERSION}/src/game/game_fn_8000C400.externalized"],
+            "rule": "externalize_game_bias_15",
+            "inputs": [f"build/{VERSION}/src/game/game_fn_8000C400.o"],
+        },
     ]
 }
 if args.map:
@@ -415,6 +420,7 @@ config.libs = [
                 "game/game_fn_8000C220.c",
                 extra_cflags=["-use_lmw_stmw on"],
             ),
+            Object(Matching, "game/game_fn_8000C400.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_8000C400.c
@@ -0,0 +1,59 @@
+typedef int s32;
+typedef signed short s16;
+
+typedef struct Vec3s {
+    s16 x;
+    s16 y;
+    s16 z;
+} Vec3s;
+
+typedef struct Vec3f {
+    float x;
+    float y;
+    float z;
+} Vec3f;
+
+extern const char lbl_8023BEF8[];
+extern s32 lbl_8064D18C;
+extern s32 fn_8016A598(void*);
+extern double fn_8016A694(void*, s32);
+extern Vec3s* fn_8016A784(void*, s32);
+extern void fn_80163BB4(void*, const char*, ...);
+extern void* fn_800CF3D4(Vec3f*, s32, s32, s32);
+extern s32 fn_80201B54(void*);
+extern void fn_8016A830(void*, double);
+
+s32 fn_8000C400(void* script)
+{
+    s32 id;
+    s32 result = 0;
+    Vec3s* source;
+    s32 type;
+    s32 final_type;
+    Vec3f position;
+    void* object;
+
+    if (fn_8016A598(script) != 3) {
+        fn_80163BB4(script, lbl_8023BEF8, 3, fn_8016A598(script));
+        return 0;
+    }
+
+    id = (s32)fn_8016A694(script, 1);
+    source = fn_8016A784(script, 2);
+    type = (s32)fn_8016A694(script, 3);
+    if (source != 0) {
+        final_type = type;
+        if (type == -1) {
+            final_type = lbl_8064D18C;
+        }
+        position.x = source->x;
+        position.y = source->y;
+        position.z = source->z;
+        object = fn_800CF3D4(&position, -1, id, final_type);
+        if (object != 0) {
+            result = fn_80201B54(object);
+        }
+    }
+    fn_8016A830(script, result);
+    return 1;
+}
