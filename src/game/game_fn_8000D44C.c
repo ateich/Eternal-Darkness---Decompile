57e8b34bcc067b8058b7a8a562d26f1940339121
psdwizzard <k.loebenberg@gmail.com>
2026-07-28 09:24:36 -0400

    Match fn_8000D44C

---
+++ src/game/game_fn_8000D44C.c
@@ -0,0 +1,39 @@
+typedef int s32;
+
+extern const char lbl_8023BEF8[];
+extern void* lbl_8064C4E4;
+extern void* lbl_8064C528;
+extern s32 fn_8016A598(void*);
+extern double fn_8016A694(void*, s32);
+extern void fn_80163BB4(void*, const char*, ...);
+extern s32 fn_8011FCE4(void*);
+extern void* fn_801E86A0(void*, s32);
+extern void fn_8011F938(void*, void*);
+extern void fn_8011EAF4(void*, s32);
+extern void fn_801387E4(s32);
+extern void fn_801387EC(s32);
+extern void fn_801261C4(void*);
+
+s32 fn_8000D44C(void* script)
+{
+    s32 index;
+    s32 value;
+    void* object;
+
+    if (fn_8016A598(script) != 1) {
+        fn_80163BB4(script, lbl_8023BEF8, 1, fn_8016A598(script));
+        return 0;
+    }
+
+    index = (s32)fn_8016A694(script, 1);
+    if (index != -1) {
+        value = fn_8011FCE4(lbl_8064C4E4);
+        object = fn_801E86A0(lbl_8064C528, index);
+        fn_8011F938(lbl_8064C4E4, object);
+        fn_8011EAF4(lbl_8064C4E4, index);
+        fn_801387E4(index);
+        fn_801387EC(value);
+        fn_801261C4(lbl_8064C4E4);
+    }
+    return 0;
+}
