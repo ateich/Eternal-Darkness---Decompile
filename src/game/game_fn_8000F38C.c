03777bd66b425ac5e09808047625981772b847bf
psdwizzard <k.loebenberg@gmail.com>
2026-07-28 12:23:46 -0400

    Match fn_8000F38C

---
+++ src/game/game_fn_8000F38C.c
@@ -0,0 +1,41 @@
+typedef unsigned char u8;
+typedef int s32;
+
+typedef struct ObjectState {
+    u8 pad0[0x1C0];
+    u8 active;
+} ObjectState;
+
+typedef struct Object {
+    u8 pad0[0x44];
+    ObjectState* state;
+    u8 pad48[0x57];
+    u8 type;
+} Object;
+
+extern const char lbl_8023BEF8[];
+extern s32 fn_8016A598(void*);
+extern void fn_80163BB4(void*, const char*, s32, s32, ...);
+extern double fn_8016A694(void*, s32);
+extern void* fn_80201814(s32);
+extern Object* fn_80201B8C(void*);
+
+s32 fn_8000F38C(void* script)
+{
+    void* entity;
+    Object* object;
+
+    if (fn_8016A598(script) != 1) {
+        fn_80163BB4(script, lbl_8023BEF8, 1, fn_8016A598(script));
+        return 0;
+    }
+
+    entity = fn_80201814((s32)fn_8016A694(script, 1));
+    if (entity != 0) {
+        object = fn_80201B8C(entity);
+        if (object->type == 5) {
+            object->state->active = 1;
+        }
+    }
+    return 0;
+}
