db08d7b9e188a59d65649669cc55bf6ee3aef6b2
psdwizzard <k.loebenberg@gmail.com>
2026-07-28 09:16:34 -0400

    Match fn_8000D2B8

--- configure.py
+++ configure.py
@@ -475,6 +475,7 @@ config.libs = [
             Object(Matching, "game/game_fn_8000D0BC.c"),
             Object(Matching, "game/game_fn_8000D188.c"),
             Object(Matching, "game/game_fn_8000D218.c"),
+            Object(Matching, "game/game_fn_8000D2B8.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_8000D2B8.c
@@ -0,0 +1,23 @@
+typedef int s32;
+
+extern const char lbl_8023BEF8[];
+extern s32 fn_8016A598(void*);
+extern double fn_8016A694(void*, s32);
+extern void fn_80163BB4(void*, const char*, ...);
+extern void fn_800DE4D8(s32, s32);
+
+s32 fn_8000D2B8(void* script)
+{
+    s32 first;
+    s32 second;
+
+    if (fn_8016A598(script) != 2) {
+        fn_80163BB4(script, lbl_8023BEF8, 2, fn_8016A598(script));
+        return 0;
+    }
+
+    first = (s32)fn_8016A694(script, 1);
+    second = (s32)fn_8016A694(script, 2);
+    fn_800DE4D8(first, second);
+    return 0;
+}
