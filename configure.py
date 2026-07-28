a1e19a0bbf2761512ff05e3453813d095e4b8ecb
psdwizzard <k.loebenberg@gmail.com>
2026-07-28 08:22:28 -0400

    Match fn_8000B900

--- configure.py
+++ configure.py
@@ -252,6 +252,11 @@ config.custom_build_steps = {
             "rule": "externalize_game_bias_18",
             "inputs": [f"build/{VERSION}/src/game/game_fn_8000B7FC.o"],
         },
+        {
+            "outputs": [f"build/{VERSION}/src/game/game_fn_8000B900.externalized"],
+            "rule": "externalize_game_bias_8",
+            "inputs": [f"build/{VERSION}/src/game/game_fn_8000B900.o"],
+        },
     ]
 }
 if args.map:
@@ -352,6 +357,7 @@ config.libs = [
                 "game/game_fn_8000B7FC.c",
                 extra_cflags=["-use_lmw_stmw on"],
             ),
+            Object(Matching, "game/game_fn_8000B900.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
