c8adf455812bc826d929eefbf149135662324f49
psdwizzard <k.loebenberg@gmail.com>
2026-07-28 14:49:38 -0400

    Match fn_80012134

--- configure.py
+++ configure.py
@@ -737,6 +737,7 @@ config.libs = [
             Object(Matching, "game/game_fn_80011F6C.c"),
             Object(Matching, "game/game_fn_80012008.c"),
             Object(Matching, "game/game_fn_80012120.c"),
+            Object(Matching, "game/game_fn_80012134.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_80012134.c
@@ -0,0 +1,10 @@
+typedef int s32;
+
+extern void* lbl_8064C4E0;
+extern void fn_801E7974(void*, s32);
+
+s32 fn_80012134(void* script)
+{
+    fn_801E7974(lbl_8064C4E0, 749);
+    return 0;
+}
