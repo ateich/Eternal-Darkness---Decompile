c1aec9aa235a6d55fc7c734ca6957e18a5788fc2
psdwizzard <k.loebenberg@gmail.com>
2026-07-28 10:00:41 -0400

    Match fn_8000DCF8

--- configure.py
+++ configure.py
@@ -526,6 +526,7 @@ config.libs = [
                 "game/game_fn_8000DBD8.c",
                 extra_cflags=["-use_lmw_stmw on"],
             ),
+            Object(Matching, "game/game_fn_8000DCF8.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_8000DCF8.c
@@ -0,0 +1,32 @@
+typedef int s32;
+
+extern const char lbl_8023BEF8[];
+extern s32 fn_8016A598(void*);
+extern double fn_8016A694(void*, s32);
+extern void fn_80163BB4(void*, const char*, ...);
+extern void* fn_80201814(s32);
+extern void* fn_80201B8C(void);
+extern s32 fn_80036D5C(void*);
+extern void fn_80036DA4(void*, s32);
+
+s32 fn_8000DCF8(void* script)
+{
+    void* object;
+    s32 object_id;
+    s32 flags;
+
+    if (fn_8016A598(script) != 2) {
+        fn_80163BB4(script, lbl_8023BEF8, 2, fn_8016A598(script));
+        return 0;
+    }
+
+    object_id = (s32)fn_8016A694(script, 1);
+    fn_8016A694(script, 2);
+    object = fn_80201814(object_id);
+    if (object != 0) {
+        fn_80201B8C();
+        flags = fn_80036D5C(object);
+        fn_80036DA4(object, flags | 0x200);
+    }
+    return 0;
+}
