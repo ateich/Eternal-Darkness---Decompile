046c5b5752c44da09d6d6ee12baacd41b9abdb32
psdwizzard <k.loebenberg@gmail.com>
2026-07-28 08:23:57 -0400

    Match fn_8000BA98

--- configure.py
+++ configure.py
@@ -365,6 +365,7 @@ config.libs = [
             Object(Matching, "game/game_fn_8000B900.c"),
             Object(Matching, "game/game_fn_8000B98C.c"),
             Object(Matching, "game/game_fn_8000BA0C.c"),
+            Object(Matching, "game/game_fn_8000BA98.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_8000BA98.c
@@ -0,0 +1,8 @@
+typedef int s32;
+
+extern s32 lbl_8064C60C;
+
+void fn_8000BA98(s32 value)
+{
+    lbl_8064C60C = value;
+}
