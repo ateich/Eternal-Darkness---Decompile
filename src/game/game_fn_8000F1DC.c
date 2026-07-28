f0f2f159f4ddadfb92e6b956cd213c8e4739d906
psdwizzard <k.loebenberg@gmail.com>
2026-07-28 12:23:46 -0400

    Document fn_8000F1DC in C

---
+++ src/game/game_fn_8000F1DC.c
@@ -0,0 +1,80 @@
+typedef signed short s16;
+typedef unsigned int u32;
+typedef int s32;
+
+#pragma use_lmw_stmw on
+
+typedef struct ObjectData {
+    unsigned char pad0[0x152];
+    s16 x;
+    s16 y;
+    s16 value;
+} ObjectData;
+
+typedef struct Object {
+    unsigned char pad0[0x8C];
+    ObjectData* data;
+} Object;
+
+extern s32 fn_8016A598(void*);
+extern double fn_8016A694(void*, s32);
+extern void* fn_80201814(s32);
+extern Object* fn_80201B8C(void*);
+extern void fn_80201ADC(Object*);
+extern void fn_800CAC5C(s32, s32, s32*, s32*, s32*);
+extern u32 fn_80036D5C(void*);
+extern void fn_80036DA4(void*, u32);
+
+s32 fn_8000F1DC(void* script)
+{
+    void* script_reg;
+    s32 arg2;
+    s32 arg1;
+    s32 arg4 = 0;
+    s32 arg3 = 0;
+    void* entity;
+    Object* object;
+    ObjectData* data;
+    s32 unused;
+    s32 x;
+    s32 y;
+    u32 flags;
+
+    script_reg = script;
+    switch (fn_8016A598(script_reg)) {
+    case 2:
+        arg1 = (s32)fn_8016A694(script_reg, 1);
+        arg2 = (s32)fn_8016A694(script_reg, 2);
+        break;
+    case 3:
+        arg1 = (s32)fn_8016A694(script_reg, 1);
+        arg2 = (s32)fn_8016A694(script_reg, 2);
+        arg3 = (s32)fn_8016A694(script_reg, 3);
+        break;
+    case 4:
+        arg1 = (s32)fn_8016A694(script_reg, 1);
+        arg2 = (s32)fn_8016A694(script_reg, 2);
+        arg3 = (s32)fn_8016A694(script_reg, 3);
+        arg4 = (s32)fn_8016A694(script_reg, 4);
+        break;
+    }
+
+    entity = fn_80201814(arg1);
+    if (entity != 0) {
+        object = fn_80201B8C(entity);
+        data = object->data;
+        fn_80201ADC(object);
+        fn_800CAC5C(5, arg2, &x, &y, &unused);
+        data->x = x;
+        data->y = y;
+        data->value = arg4;
+        flags = fn_80036D5C(entity);
+        if (arg3 != 0) {
+            flags = (flags | 0x8000) & ~0x80;
+        } else {
+            flags = (flags | 0x80) & ~0x8000;
+        }
+        fn_80036DA4(entity, flags);
+    }
+    return 0;
+}
