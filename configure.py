522a555072c6561797083e441035d1531c1f617e
psdwizzard <k.loebenberg@gmail.com>
2026-07-29 10:35:43 -0400

    Match fn_800131E8

--- configure.py
+++ configure.py
@@ -759,6 +759,7 @@ config.libs = [
             Object(Matching, "game/game_fn_80012FD0.c"),
             Object(Matching, "game/game_fn_80013098.c"),
             Object(Matching, "game/game_fn_80013154.c"),
+            Object(Matching, "game/game_fn_800131E8.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_800131E8.c
@@ -0,0 +1,34 @@
+typedef int s32;
+
+extern const char lbl_8023BEF8[];
+extern const double lbl_8064DCE8;
+extern void* fn_80201B3C(void);
+extern s32 fn_8016A598(void*);
+extern void fn_80163BB4(void*, const char*, s32, s32, ...);
+extern void* fn_80049220(void*, s32);
+extern void* fn_80049304(void*, void*);
+extern s32 fn_80201B54(void*);
+extern void fn_8016A830(void*, double);
+
+s32 fn_800131E8(void* script)
+{
+    register s32 value = -1;
+    register void* context;
+    register void* parser = script;
+    void* temporary;
+    void* object;
+
+    context = fn_80201B3C();
+    if (fn_8016A598(parser) != 0) {
+        fn_80163BB4(parser, lbl_8023BEF8, 0, fn_8016A598(parser));
+        return 0;
+    }
+
+    temporary = fn_80049220(context, 1);
+    object = fn_80049304(context, temporary);
+    if (object != 0) {
+        value = fn_80201B54(object);
+    }
+    fn_8016A830(parser, (double)value);
+    return 1;
+}
