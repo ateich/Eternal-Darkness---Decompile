3ca289a73e50812cc50c602d38ff79a236126614
psdwizzard <k.loebenberg@gmail.com>
2026-07-29 12:35:34 -0400

    Match fn_80016128

--- configure.py
+++ configure.py
@@ -607,6 +607,11 @@ config.custom_build_steps = {
             "rule": "externalize_game_bias_19",
             "inputs": [f"build/{VERSION}/src/game/game_fn_80015F04.o"],
         },
+        {
+            "outputs": [f"build/{VERSION}/src/game/game_fn_80016128.externalized"],
+            "rule": "externalize_game_bias_10",
+            "inputs": [f"build/{VERSION}/src/game/game_fn_80016128.o"],
+        },
     ]
 }
 if args.map:
@@ -971,6 +976,7 @@ config.libs = [
                 "game/game_fn_80015F04.c",
                 extra_cflags=["-use_lmw_stmw on"],
             ),
+            Object(Matching, "game/game_fn_80016128.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_80016128.c
@@ -0,0 +1,42 @@
+typedef int s32;
+typedef signed short s16;
+
+typedef struct Vec3f {
+    float x;
+    float y;
+    float z;
+} Vec3f;
+
+extern const char lbl_8023BEF8[];
+extern const char lbl_8023CB0C[];
+extern float lbl_8064DE5C;
+extern s32 fn_8016A598(void*);
+extern s16* fn_8016A784(void*, s32);
+extern void fn_80163BB4(void*, const char*, s32, ...);
+extern void fn_80152838(Vec3f*, Vec3f*, s32);
+
+s32 fn_80016128(void* script)
+{
+    s16* position;
+    Vec3f start;
+    Vec3f end;
+
+    if (fn_8016A598(script) != 1) {
+        fn_80163BB4(script, lbl_8023BEF8, 1, fn_8016A598(script));
+        return 0;
+    }
+
+    position = fn_8016A784(script, 1);
+    if (position == 0) {
+        fn_80163BB4(script, lbl_8023CB0C, 0);
+    }
+
+    start.x = position[0];
+    start.y = position[1];
+    start.z = position[2];
+    end.x = position[0];
+    end.y = position[1];
+    end.z = lbl_8064DE5C + position[2];
+    fn_80152838(&start, &end, 4);
+    return 0;
+}
