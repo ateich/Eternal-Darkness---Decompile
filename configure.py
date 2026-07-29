0dc64dc9685de9ece0edd1440cf015ceacb1d598
psdwizzard <k.loebenberg@gmail.com>
2026-07-29 13:57:14 -0400

    Match fn_80017260

--- configure.py
+++ configure.py
@@ -1038,6 +1038,7 @@ config.libs = [
             Object(Matching, "game/game_fn_80017074.c"),
             Object(Matching, "game/game_fn_80017134.c"),
             Object(Matching, "game/game_fn_800171C0.c"),
+            Object(Matching, "game/game_fn_80017260.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_80017260.c
@@ -0,0 +1,37 @@
+typedef int s32;
+
+extern const char lbl_8023BEF8[];
+extern void* lbl_8064C504;
+
+extern s32 fn_8016A598(void*);
+extern double fn_8016A694(void*, s32);
+extern void fn_80163BB4(void*, const char*, s32, ...);
+extern void* fn_801E6CA0(void*, s32, s32, s32, s32);
+extern void fn_80027730(void*, s32, s32);
+
+s32 fn_80017260(void* script)
+{
+    if (fn_8016A598(script) != 1) {
+        fn_80163BB4(script, lbl_8023BEF8, 1, fn_8016A598(script));
+        return 0;
+    }
+
+    switch ((s32)fn_8016A694(script, 1)) {
+    case 2:
+        fn_80027730(fn_801E6CA0(lbl_8064C504, 0, 0x2E, 0, 1), 0, 0);
+        break;
+    case 3:
+        fn_80027730(fn_801E6CA0(lbl_8064C504, 0, 0x2F, 0, 1), 0, 0);
+        break;
+    case 4:
+        fn_80027730(fn_801E6CA0(lbl_8064C504, 0, 0x30, 0, 1), 0, 0);
+        break;
+    case 5:
+        fn_80027730(fn_801E6CA0(lbl_8064C504, 0, 0x31, 0, 1), 0, 0);
+        break;
+    case 6:
+        fn_80027730(fn_801E6CA0(lbl_8064C504, 0, 0x32, 0, 1), 0, 0);
+        break;
+    }
+    return 1;
+}
