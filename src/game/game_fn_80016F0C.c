1f5e7e053c66279ae682c13101c25e91aa5f2517
psdwizzard <k.loebenberg@gmail.com>
2026-07-29 13:40:41 -0400

    Match fn_80016F0C

---
+++ src/game/game_fn_80016F0C.c
@@ -0,0 +1,20 @@
+typedef int s32;
+
+extern const char lbl_8023BEF8[];
+
+extern s32 fn_8016A598(void*);
+extern void fn_80163BB4(void*, const char*, s32, ...);
+extern void fn_8010BFF0(void);
+extern void fn_8010AD7C(void);
+
+s32 fn_80016F0C(void* script)
+{
+    if (fn_8016A598(script) != 0) {
+        fn_80163BB4(script, lbl_8023BEF8, 0, fn_8016A598(script));
+        return 0;
+    }
+
+    fn_8010BFF0();
+    fn_8010AD7C();
+    return 0;
+}
