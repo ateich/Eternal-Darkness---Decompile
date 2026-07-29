4930998a5a8b53c3bbca1825d2b2d7764ce85c44
psdwizzard <k.loebenberg@gmail.com>
2026-07-29 11:24:41 -0400

    Match fn_800141B4

---
+++ src/game/game_fn_800141B4.c
@@ -0,0 +1,18 @@
+typedef int s32;
+
+extern const char lbl_8023BEF8[];
+extern s32 fn_8016A598(void*);
+extern double fn_8016A694(void*, s32);
+extern void fn_80163BB4(void*, const char*, s32, s32, ...);
+extern void fn_801D13D8(s32, s32);
+
+s32 fn_800141B4(void* script)
+{
+    if (fn_8016A598(script) != 1) {
+        fn_80163BB4(script, lbl_8023BEF8, 1, fn_8016A598(script));
+        return 0;
+    }
+
+    fn_801D13D8((s32)fn_8016A694(script, 1), 1);
+    return 0;
+}
