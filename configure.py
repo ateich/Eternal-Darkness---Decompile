7e860beba5798fba735fa1303364963e6ee75418
psdwizzard <k.loebenberg@gmail.com>
2026-07-29 10:34:53 -0400

    Match fn_80013154

--- configure.py
+++ configure.py
@@ -758,6 +758,7 @@ config.libs = [
             Object(Matching, "game/game_fn_80012F40.c"),
             Object(Matching, "game/game_fn_80012FD0.c"),
             Object(Matching, "game/game_fn_80013098.c"),
+            Object(Matching, "game/game_fn_80013154.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_80013154.c
@@ -0,0 +1,20 @@
+typedef int s32;
+
+extern double fn_8016A694(void*, s32);
+extern s32 fn_80201B44(void);
+extern void fn_800CC140(s32, s32, s32, s32, s32);
+
+s32 fn_80013154(void* script)
+{
+    s32 first;
+    s32 second;
+
+    first = (s32)fn_8016A694(script, 1);
+    second = (s32)fn_8016A694(script, 2);
+    if (second != 0) {
+        fn_800CC140(fn_80201B44(), first, 0, 2, 0);
+    } else {
+        fn_800CC140(fn_80201B44(), 0, 0, 3, 0);
+    }
+    return 0;
+}
