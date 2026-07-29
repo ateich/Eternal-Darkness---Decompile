d487bf72f290f38bcf4df779a6b73a4dd7eede39
psdwizzard <k.loebenberg@gmail.com>
2026-07-29 10:31:39 -0400

    Match fn_80012E34

--- configure.py
+++ configure.py
@@ -754,6 +754,7 @@ config.libs = [
             Object(Matching, "game/game_fn_80012B6C.c"),
             Object(Matching, "game/game_fn_80012CC4.c"),
             Object(Matching, "game/game_fn_80012DA8.c"),
+            Object(Matching, "game/game_fn_80012E34.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_80012E34.c
@@ -0,0 +1,40 @@
+typedef int s32;
+
+extern const char lbl_8023BEF8[];
+extern const char lbl_8023CA98[];
+extern s32 fn_8016A598(void*);
+extern double fn_8016A694(void*, s32);
+extern void fn_80163BB4(void*, const char*, ...);
+extern void* fn_80201814(s32);
+extern void fn_800355F0(void*, s32);
+extern s32 fn_80201B44(void);
+extern void fn_800427E0(s32);
+
+s32 fn_80012E34(void* script)
+{
+    register s32 object_id;
+    register s32 mode;
+    register void* object;
+    register void* parser = script;
+
+    if (fn_8016A598(parser) != 2) {
+        fn_80163BB4(parser, lbl_8023BEF8, 2, fn_8016A598(parser));
+        return 0;
+    }
+
+    object_id = (s32)fn_8016A694(parser, 1);
+    object = fn_80201814(object_id);
+    if (object != 0) {
+        mode = (s32)fn_8016A694(parser, 2);
+        if (mode < 0 || mode > 4) {
+            fn_80163BB4(parser, lbl_8023CA98, mode);
+            return 0;
+        }
+
+        fn_800355F0(object, mode);
+        if (fn_80201B44() == object_id) {
+            fn_800427E0(mode);
+        }
+    }
+    return 0;
+}
