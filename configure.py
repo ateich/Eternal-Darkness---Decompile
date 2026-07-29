89fe3d4950afd345a504524e85227137d69877be
psdwizzard <k.loebenberg@gmail.com>
2026-07-29 11:55:06 -0400

    Match fn_800152B8

--- configure.py
+++ configure.py
@@ -902,6 +902,7 @@ config.libs = [
                 "game/game_fn_80015058.c",
                 extra_cflags=["-use_lmw_stmw on"],
             ),
+            Object(Matching, "game/game_fn_800152B8.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
