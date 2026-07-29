065719befb36ceb973f0adc10b6d0a9d489eb268
psdwizzard <k.loebenberg@gmail.com>
2026-07-29 15:37:33 -0400

    Match fn_80018ED4

--- configure.py
+++ configure.py
@@ -1118,6 +1118,7 @@ config.libs = [
                 "game/game_fn_80018CC4.c",
                 extra_cflags=["-use_lmw_stmw on"],
             ),
+            Object(Matching, "game/game_fn_80018ED4.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_80018ED4.c
@@ -0,0 +1,6 @@
+extern void fn_8014B928(void);
+
+void fn_80018ED4(void)
+{
+    fn_8014B928();
+}
