c21a0701ef4f3e04a3ddeca4eceaaffa978f9265
psdwizzard <k.loebenberg@gmail.com>
2026-07-29 11:17:08 -0400

    Match fn_80013EEC

---
+++ src/game/game_fn_80013EEC.c
@@ -0,0 +1,21 @@
+typedef int s32;
+
+extern const char lbl_8023BEF8[];
+extern s32 fn_8016A598(void*);
+extern double fn_8016A694(void*, s32);
+extern void fn_80163BB4(void*, const char*, s32, s32, ...);
+extern void fn_801D16E4(s32, s32);
+
+s32 fn_80013EEC(void* script)
+{
+    s32 first;
+
+    if (fn_8016A598(script) != 2) {
+        fn_80163BB4(script, lbl_8023BEF8, 2, fn_8016A598(script));
+        return 0;
+    }
+
+    first = (s32)fn_8016A694(script, 1);
+    fn_801D16E4(first, (s32)fn_8016A694(script, 2));
+    return 0;
+}
