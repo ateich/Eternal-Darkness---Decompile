406fb9830d276752ff9bf5bf02e8aad27746eacc
psdwizzard <k.loebenberg@gmail.com>
2026-07-29 12:46:19 -0400

    Match fn_80016414

--- configure.py
+++ configure.py
@@ -987,6 +987,11 @@ config.libs = [
                 "game/game_fn_80016270.c",
                 extra_cflags=["-use_lmw_stmw on"],
             ),
+            Object(
+                Matching,
+                "game/game_fn_80016414.c",
+                extra_cflags=["-use_lmw_stmw on"],
+            ),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_80016414.c
@@ -0,0 +1,64 @@
+typedef int s32;
+typedef unsigned char u8;
+typedef signed short s16;
+
+typedef struct SpawnInfo {
+    u8 unk0;
+    u8 unk1;
+    u8 unk2[4];
+    s16 unk6;
+    u8 unk8[19];
+    u8 unk27;
+    u8 unk28[116];
+} SpawnInfo;
+
+extern const char lbl_8023BEF8[];
+extern s32 fn_8016A598(void*);
+extern double fn_8016A694(void*, s32);
+extern void fn_80163BB4(void*, const char*, s32, ...);
+extern void fn_8015319C(s32, s32, s32, s32, s32, s32, s32);
+extern void fn_80152BCC(s32, s32, s32, volatile SpawnInfo*, s32, s32, s32,
+                       s32);
+
+s32 fn_80016414(void* script)
+{
+    s32 fifth;
+    s32 fourth;
+    s32 i;
+    s32 first;
+    s32 second;
+    s32 third;
+    s32 sixth;
+    s32 seventh;
+    s32 eighth;
+    volatile SpawnInfo info;
+
+    if (fn_8016A598(script) != 8) {
+        fn_80163BB4(script, lbl_8023BEF8, 8, fn_8016A598(script));
+        return 0;
+    }
+
+    first = (s32)fn_8016A694(script, 1);
+    second = (s32)fn_8016A694(script, 2);
+    third = (s32)fn_8016A694(script, 3);
+    fourth = (s32)fn_8016A694(script, 4);
+    fifth = (s32)fn_8016A694(script, 5);
+    sixth = (s32)fn_8016A694(script, 6);
+    seventh = (s32)fn_8016A694(script, 7);
+    eighth = (s32)fn_8016A694(script, 8);
+
+    if ((u8)eighth != 0) {
+        fn_8015319C(first, second, third, fourth, fifth, sixth, seventh);
+    } else {
+        info.unk6 = (s16)fifth;
+        fourth = (u8)fourth;
+        i = 0;
+        info.unk1 = (u8)sixth;
+        info.unk27 = (u8)seventh;
+        while (i < fourth) {
+            fn_80152BCC(first, second, third, &info, 17, 1, 1, 128);
+            i++;
+        }
+    }
+    return 0;
+}
