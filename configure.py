adcf2226ef1a69a2625cff501a7de1086f6c631f
psdwizzard <k.loebenberg@gmail.com>
2026-07-29 10:56:40 -0400

    Match fn_800136C4

--- configure.py
+++ configure.py
@@ -794,6 +794,7 @@ config.libs = [
             ),
             Object(Matching, "game/game_fn_80013564.c"),
             Object(Matching, "game/game_fn_80013614.c"),
+            Object(Matching, "game/game_fn_800136C4.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_800136C4.c
@@ -0,0 +1,21 @@
+typedef int s32;
+
+extern const char lbl_8023BEF8[];
+extern s32 fn_8016A598(void*);
+extern double fn_8016A694(void*, s32);
+extern void fn_80163BB4(void*, const char*, s32, s32, ...);
+extern void* fn_80201814(s32);
+extern void fn_80204FDC(void*);
+
+s32 fn_800136C4(void* script)
+{
+    void* parser = script;
+
+    if (fn_8016A598(parser) != 1) {
+        fn_80163BB4(parser, lbl_8023BEF8, 1, fn_8016A598(parser));
+        return 0;
+    }
+
+    fn_80204FDC(fn_80201814((s32)fn_8016A694(parser, 1)));
+    return 0;
+}
