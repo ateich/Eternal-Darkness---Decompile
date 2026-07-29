35865a50676fe906dfab14c4f0b74a1e8e630ea0
psdwizzard <k.loebenberg@gmail.com>
2026-07-29 15:00:21 -0400

    Match fn_80017C3C

--- configure.py
+++ configure.py
@@ -328,6 +328,11 @@ config.custom_build_rules = [
 config.custom_build_steps = {
     "post-compile": [
         {
+            "outputs": [f"build/{VERSION}/src/game/game_fn_80017C3C.externalized"],
+            "rule": "externalize_game_bias_8",
+            "inputs": [f"build/{VERSION}/src/game/game_fn_80017C3C.o"],
+        },
+        {
             "outputs": [f"build/{VERSION}/src/game/game_fn_80017AE4.externalized"],
             "rule": "externalize_game_bias_8",
             "inputs": [f"build/{VERSION}/src/game/game_fn_80017AE4.o"],
@@ -1089,6 +1094,7 @@ config.libs = [
             Object(Matching, "game/game_fn_80017A3C.c"),
             Object(Matching, "game/game_fn_80017AE4.c"),
             Object(Matching, "game/game_fn_80017B80.c"),
+            Object(Matching, "game/game_fn_80017C3C.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
