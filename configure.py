65919c7c262f79ea75957b85e80b93aabd349e64
psdwizzard <k.loebenberg@gmail.com>
2026-07-29 17:14:53 -0400

    Match fn_80019B88

--- configure.py
+++ configure.py
@@ -1165,6 +1165,7 @@ config.libs = [
             Object(Matching, "game/game_fn_800198A4.c"),
             Object(Matching, "game/game_fn_80019940.c"),
             Object(Matching, "game/game_fn_80019B08.c"),
+            Object(Matching, "game/game_fn_80019B88.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
