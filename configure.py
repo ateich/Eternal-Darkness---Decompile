44bb8afa90034b6c5d060e15dc87ee116066c2c0
psdwizzard <k.loebenberg@gmail.com>
2026-08-03 08:27:43 -0400

    Match fn_8001A600

--- configure.py
+++ configure.py
@@ -1194,6 +1194,7 @@ config.libs = [
             Object(Matching, "game/game_fn_8001A4C8.c"),
             Object(Matching, "game/game_fn_8001A568.c"),
             Object(Matching, "game/game_fn_8001A594.c"),
+            Object(Matching, "game/game_fn_8001A600.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_8001A600.c
@@ -0,0 +1,9 @@
+typedef int s32;
+
+extern void fn_800AFEC0(void);
+
+s32 fn_8001A600(void)
+{
+    fn_800AFEC0();
+    return 0;
+}
