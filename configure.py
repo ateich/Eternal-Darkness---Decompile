4923c396ac2126823c99688e87394a5eed9248a0
psdwizzard <k.loebenberg@gmail.com>
2026-08-03 12:43:04 -0400

    Match fn_80020D70

--- configure.py
+++ configure.py
@@ -1405,6 +1405,7 @@ config.libs = [
             Object(NonMatching, "game/game_fn_8001E644.c"),
             Object(Matching, "game/game_fn_8001F754.c"),
             Object(Matching, "game/game_fn_8002014C.c"),
+            Object(Matching, "game/game_fn_80020D70.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_80020D70.c
@@ -0,0 +1,6 @@
+extern void fn_800B18F8(void);
+
+void fn_80020D70(void)
+{
+    fn_800B18F8();
+}
