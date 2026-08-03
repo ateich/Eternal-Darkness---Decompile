0d944cc338d9599c51fc0169e0e333e20a6469b0
psdwizzard <k.loebenberg@gmail.com>
2026-08-03 11:47:07 -0400

    Match fn_8001DE68

--- configure.py
+++ configure.py
@@ -1395,6 +1395,7 @@ config.libs = [
             Object(Matching, "game/game_fn_8001DA18.c"),
             Object(Matching, "game/game_fn_8001DA7C.c"),
             Object(Matching, "game/game_fn_8001DB04.c", extra_cflags=["-use_lmw_stmw on"]),
+            Object(Matching, "game/game_fn_8001DE68.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_8001DE68.c
@@ -0,0 +1,12 @@
+typedef unsigned int u32;
+
+extern u32 lbl_8064C6B4[2];
+extern u32 lbl_8064C6BC[2];
+
+void fn_8001DE68(void)
+{
+    lbl_8064C6BC[0] = 0;
+    lbl_8064C6BC[0] = 0;
+    lbl_8064C6B4[0] = 0;
+    lbl_8064C6B4[1] = 0;
+}
