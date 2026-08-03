280373f4b356cf0dc4736ef1c50a53c62f4d6be1
psdwizzard <k.loebenberg@gmail.com>
2026-08-03 11:08:02 -0400

    Match fn_8001D2EC

--- configure.py
+++ configure.py
@@ -1380,6 +1380,7 @@ config.libs = [
             Object(Matching, "game/game_fn_8001D11C.c"),
             Object(Matching, "game/game_fn_8001D130.c"),
             Object(NonMatching, "game/game_fn_8001D1BC.c"),
+            Object(Matching, "game/game_fn_8001D2EC.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_8001D2EC.c
@@ -0,0 +1,20 @@
+typedef int s32;
+typedef float f32;
+
+extern s32 fn_8016A598(void*);
+extern double fn_8016A694(void*, s32);
+extern void fn_80163BB4(void*, const char*, s32, ...);
+extern void fn_800C3CE4(f32);
+extern const f32 lbl_8064DE90;
+extern char lbl_8023BEF8[];
+
+s32 fn_8001D2EC(void* script)
+{
+    if (fn_8016A598(script) != 1) {
+        fn_80163BB4(script, lbl_8023BEF8, 1, fn_8016A598(script));
+        return 0;
+    }
+
+    fn_800C3CE4(lbl_8064DE90 * (f32)fn_8016A694(script, 1));
+    return 0;
+}
