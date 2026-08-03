6d7156bfe022a3081612dbc3e9683d056bb42887
psdwizzard <k.loebenberg@gmail.com>
2026-08-03 10:15:26 -0400

    Match fn_8001C310

--- configure.py
+++ configure.py
@@ -1340,6 +1340,7 @@ config.libs = [
                 extra_cflags=["-use_lmw_stmw on"],
             ),
             Object(Matching, "game/game_fn_8001C250.c"),
+            Object(Matching, "game/game_fn_8001C310.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_8001C310.c
@@ -0,0 +1,24 @@
+typedef unsigned char u8;
+typedef unsigned int u32;
+typedef int s32;
+
+typedef struct Command {
+    u32 unknown0;
+    void* context;
+    s32 unknown8;
+    u32 id;
+    s32 value;
+} Command;
+
+extern double fn_8016A694(void*, s32);
+extern u32 fn_800F5C54(double);
+extern Command lbl_803023E0;
+extern void* lbl_8064D1BC;
+
+void fn_8001C310(void* arg)
+{
+    lbl_803023E0.unknown8 = 0;
+    lbl_803023E0.context = lbl_8064D1BC;
+    lbl_803023E0.id = fn_800F5C54(fn_8016A694(arg, 1));
+    lbl_803023E0.value = (s32)fn_8016A694(arg, 2);
+}
