305f1036f375be8bfb7849909d26df8d8d2c5ec2
psdwizzard <k.loebenberg@gmail.com>
2026-08-03 08:57:42 -0400

    Match fn_8001AC70

--- configure.py
+++ configure.py
@@ -1220,6 +1220,7 @@ config.libs = [
             Object(Matching, "game/game_fn_8001AAB8.c"),
             Object(Matching, "game/game_fn_8001AB70.c"),
             Object(Matching, "game/game_fn_8001ABF0.c"),
+            Object(Matching, "game/game_fn_8001AC70.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_8001AC70.c
@@ -0,0 +1,30 @@
+typedef int s32;
+
+extern const char lbl_8023BEF8[];
+
+extern s32 fn_8016A598(void*);
+extern double fn_8016A694(void*, s32);
+extern void fn_80163BB4(void*, const char*, s32, ...);
+extern void fn_80087840(s32, float, s32, float, s32);
+
+s32 fn_8001AC70(void* script)
+{
+    s32 arg1;
+    float arg2;
+    float arg4;
+    s32 arg3;
+    s32 arg5;
+
+    if (fn_8016A598(script) != 5) {
+        fn_80163BB4(script, lbl_8023BEF8, 5, fn_8016A598(script));
+        return 0;
+    }
+
+    arg1 = (s32)fn_8016A694(script, 1);
+    arg2 = (float)fn_8016A694(script, 2);
+    arg4 = (float)fn_8016A694(script, 3);
+    arg3 = (s32)fn_8016A694(script, 4);
+    arg5 = (s32)fn_8016A694(script, 5);
+    fn_80087840(arg1, arg2, arg3, arg4, arg5);
+    return 0;
+}
