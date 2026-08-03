2fc7446d7af81087d5abfc6b49257c009f6a9f67
psdwizzard <k.loebenberg@gmail.com>
2026-08-03 10:13:35 -0400

    Match fn_8001C250

---
+++ src/game/game_fn_8001C250.c
@@ -0,0 +1,40 @@
+typedef unsigned char u8;
+typedef int s32;
+
+typedef struct Ref Ref;
+
+typedef struct State {
+    u8 pad[0x7C];
+    s32 value;
+} State;
+
+typedef struct Object {
+    State* state;
+} Object;
+
+extern s32 fn_8016A598(void*);
+extern double fn_8016A694(void*, s32);
+extern void fn_80163BB4(void*, const char*, s32, ...);
+extern s32 fn_80201B44(void);
+extern Ref* fn_80201814(s32);
+extern Object* fn_80201B8C(Ref*);
+extern char lbl_8023BEF8[];
+
+s32 fn_8001C250(void* arg)
+{
+    Ref* ref = fn_80201814(fn_80201B44());
+    s32 value;
+
+    if (fn_8016A598(arg) != 1) {
+        fn_80163BB4(arg, lbl_8023BEF8, 1, fn_8016A598(arg));
+        return 0;
+    }
+
+    value = (s32)fn_8016A694(arg, 1);
+    if (ref != 0) {
+        Object* object = fn_80201B8C(ref);
+        State* state = object != 0 ? object->state : 0;
+        state->value = value;
+    }
+    return 0;
+}
