4667f3cc257de6bdc05492befc54e69e6e961e70
psdwizzard <k.loebenberg@gmail.com>
2026-08-03 12:50:11 -0400

    Match fn_800236A0

---
+++ src/game/game_fn_800236A0.c
@@ -0,0 +1,23 @@
+typedef unsigned int u32;
+
+extern u32 lbl_8030241C[];
+extern int lbl_8064B2C0;
+extern void* lbl_8064C650;
+
+extern void fn_800B18F8(void);
+extern u32 fn_801A98F4(int, int);
+extern void fn_8001DE84(int, int);
+extern void fn_801A9964(void*);
+
+void fn_800236A0(void)
+{
+    fn_800B18F8();
+    lbl_8030241C[14] = fn_801A98F4(0x277, 100);
+    lbl_8030241C[10] = 5;
+    if (lbl_8064B2C0 != 0) {
+        fn_8001DE84(7, 2);
+    } else {
+        fn_8001DE84(7, 0);
+    }
+    fn_801A9964(lbl_8064C650);
+}
