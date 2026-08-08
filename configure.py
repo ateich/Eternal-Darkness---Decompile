fb168e9a37fe3d03b3ae0f447df6d5904f56d529
psdwizzard <k.loebenberg@gmail.com>
2026-08-07 21:52:19 -0400

    Match fn_80024570

--- configure.py
+++ configure.py
@@ -1427,6 +1427,7 @@ config.libs = [
             Object(Matching, "game/game_fn_80024468.c"),
             Object(Matching, "game/game_fn_800244C4.c"),
             Object(Matching, "game/game_fn_800244F4.c"),
+            Object(Matching, "game/game_fn_80024570.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_80024570.c
@@ -0,0 +1,10 @@
+typedef unsigned int u32;
+
+extern void fn_80024594(void*, u32*);
+
+void fn_80024570(void* object)
+{
+    u32 result;
+
+    fn_80024594(object, &result);
+}
