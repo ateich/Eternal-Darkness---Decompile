59e01d3d0bc6efa8fe5f65ec7593e500957320cc
psdwizzard <k.loebenberg@gmail.com>
2026-08-08 14:32:38 -0400

    game: match fn_8002A4C8

---
+++ src/game/game_fn_8002A4C8.c
@@ -0,0 +1,12 @@
+extern void* fn_80156938(void*);
+extern void fn_80036E8C(void);
+extern void fn_80201750(void*);
+
+void fn_8002A4C8(void* callback)
+{
+    if (callback != 0) {
+        void* object = fn_80156938(callback);
+        fn_80036E8C();
+        fn_80201750(object);
+    }
+}
