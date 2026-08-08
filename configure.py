66e17ba29f53166f0d8cb3a9d87af3a8318e4bfa
psdwizzard <k.loebenberg@gmail.com>
2026-08-08 13:33:24 -0400

    Match fn_80027F3C

--- configure.py
+++ configure.py
@@ -1528,6 +1528,7 @@ config.libs = [
             Object(Matching, "game/game_fn_80027E34.c"),
             Object(Matching, "game/game_fn_80027F0C.c"),
             Object(Matching, "game/game_fn_80027F10.c"),
+            Object(Matching, "game/game_fn_80027F3C.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_80027F3C.c
@@ -0,0 +1,8 @@
+typedef int s32;
+
+extern s32 lbl_8064C708;
+
+s32 fn_80027F3C(void)
+{
+    return lbl_8064C708;
+}
