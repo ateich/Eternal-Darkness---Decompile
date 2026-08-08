a4d1deb9af56ef96ec88c2ba712983d1ff46dd8b
psdwizzard <k.loebenberg@gmail.com>
2026-08-08 13:49:30 -0400

    game: match fn_80028B44

---
+++ src/game/game_fn_80028B44.c
@@ -0,0 +1,28 @@
+typedef unsigned int u32;
+typedef unsigned short u16;
+
+extern int lbl_8064C6FC;
+extern void* fn_80201B3C(void);
+extern void* fn_80201B9C(void);
+extern void* fn_80201BC0(void*);
+extern int fn_80201B4C(void*);
+extern u32 fn_80036D5C(void*);
+
+u16 fn_80028B44(void)
+{
+    u16 count = 0;
+    void* head = fn_80201B3C();
+    void* item = fn_80201B9C();
+
+    while (item != 0) {
+        if (item != head && fn_80201B4C(item) == 1 &&
+            (fn_80036D5C(item) & 0x04000000)) {
+            count++;
+        }
+        item = fn_80201BC0(item);
+    }
+    if (count != 0) {
+        lbl_8064C6FC = 2;
+    }
+    return count;
+}
