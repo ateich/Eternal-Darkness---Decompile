b09615cd5c8b1178cd56a7c4d430a0f3cdc34c26
psdwizzard <k.loebenberg@gmail.com>
2026-08-07 20:51:43 -0400

    Match fn_80023EA8

--- configure.py
+++ configure.py
@@ -1417,6 +1417,7 @@ config.libs = [
             Object(Matching, "game/game_fn_80023B40.c", extra_cflags=["-use_lmw_stmw on"]),
             Object(Matching, "game/game_fn_80023C20.c"),
             Object(Matching, "game/game_fn_80023CF8.c"),
+            Object(Matching, "game/game_fn_80023EA8.c"),
             Object(Matching, "game/game_fn_80008B38.c"),
             Object(Matching, "game/game_fn_80008B6C.c"),
             Object(Matching, "game/game_fn_80008BD8.c"),
---
+++ src/game/game_fn_80023EA8.c
@@ -0,0 +1,8 @@
+extern void fn_800FBFA8(int value);
+extern void fn_801E81C0(int value);
+
+void fn_80023EA8(void)
+{
+    fn_800FBFA8(0);
+    fn_801E81C0(0);
+}
