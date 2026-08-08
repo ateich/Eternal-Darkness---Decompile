20070ec935a4622b5e298b3b007760be5f701bc2
psdwizzard <k.loebenberg@gmail.com>
2026-08-07 21:42:22 -0400

    Match fn_80024434

---
+++ src/game/game_fn_80024434.c
@@ -0,0 +1,13 @@
+typedef unsigned char u8;
+
+extern volatile u8 lbl_8064C368;
+
+extern void fn_8022CED0(int);
+
+void fn_80024434(void)
+{
+    lbl_8064C368;
+    if (lbl_8064C368 == 0) {
+        fn_8022CED0(0);
+    }
+}
