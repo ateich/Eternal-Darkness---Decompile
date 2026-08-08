c2fdf9c8807d0d77ab8060446a91091d2aedc32f
psdwizzard <k.loebenberg@gmail.com>
2026-08-08 14:08:01 -0400

    Match fn_80028E88

---
+++ src/game/game_fn_80028E88.c
@@ -0,0 +1,122 @@
+typedef int s32;
+
+typedef struct AlignmentRecord {
+    s32 value;
+    s32 unknown_04;
+    s32 alternate;
+} AlignmentRecord;
+
+extern const char* lbl_8023E28C[];
+extern AlignmentRecord lbl_8023E118[];
+extern s32 lbl_8064D564;
+extern s32 lbl_803003C8[];
+extern s32 lbl_8064C538;
+extern s32 lbl_8064CD34;
+
+extern char lbl_8064B37C;
+extern char lbl_8064B380;
+extern char lbl_8064B384;
+extern char lbl_8064B38C;
+extern char lbl_8064B390;
+extern char lbl_8064B398;
+extern char lbl_8064B39C;
+
+extern s32 fn_80045240(s32);
+extern void fn_800FD39C(char*, const char*);
+extern void fn_800FD40C(char*, const char*);
+extern s32 fn_80036E50(void);
+extern void* fn_80201B3C(void);
+extern s32 fn_801E394C(const char*);
+
+const char* fn_80028E88(const signed char* command, s32* result)
+{
+    const char* text = 0;
+
+    switch (*command) {
+    case 'c':
+        text = lbl_8023E28C[1];
+        break;
+    case 'u':
+        text = lbl_8023E28C[2];
+        break;
+    case 'x':
+        text = lbl_8023E28C[3];
+        break;
+    case 'm':
+        text = lbl_8023E28C[4];
+        break;
+    case 'a':
+        text = lbl_8023E28C[lbl_8064D564];
+        break;
+    case 's':
+        text = lbl_8023E28C[lbl_8023E118[lbl_8064D564].value];
+        break;
+    case 'd':
+        text = lbl_8023E28C[lbl_8023E118[lbl_8064D564].alternate];
+        break;
+    case 'g':
+    {
+        char buffer[0x70];
+
+        fn_800FD40C(buffer, &lbl_8064B37C);
+        if (fn_80045240(1) != 0) {
+            fn_800FD39C(buffer, &lbl_8064B380);
+            fn_800FD39C(buffer, lbl_8023E28C[1]);
+            fn_800FD39C(buffer, &lbl_8064B384);
+        }
+        if (fn_80045240(2) != 0) {
+            fn_800FD39C(buffer, &lbl_8064B38C);
+            fn_800FD39C(buffer, lbl_8023E28C[2]);
+            fn_800FD39C(buffer, &lbl_8064B390);
+        }
+        if (fn_80045240(3) != 0) {
+            fn_800FD39C(buffer, &lbl_8064B398);
+            fn_800FD39C(buffer, lbl_8023E28C[3]);
+            fn_800FD39C(buffer, &lbl_8064B39C);
+        }
+        {
+            const char** strings = lbl_8023E28C;
+            fn_800FD40C((char*)strings[23], buffer);
+            text = strings[23];
+        }
+        break;
+    }
+    case 'p':
+        if (fn_80201B3C() != 0) {
+            switch (fn_80036E50()) {
+            case 6:
+                text = lbl_8023E28C[20];
+                break;
+            case 3:
+                text = lbl_8023E28C[21];
+                break;
+            case 4:
+                text = lbl_8023E28C[22];
+                break;
+            case 1:
+                text = lbl_8023E28C[8 + lbl_803003C8[0]];
+                break;
+            }
+        } else {
+            text = lbl_8023E28C[8 + lbl_803003C8[0]];
+        }
+        break;
+    case 'r': {
+        s32 value = lbl_8064C538;
+        s32 positive = value > 0 ? value : 0;
+        value = positive > 1 ? 1 : (value > 0 ? value : 0);
+        lbl_8064C538 = value;
+        text = lbl_8023E28C[5 + value];
+        break;
+    }
+    case 'z':
+        text = lbl_8023E28C[24 + lbl_8064CD34];
+        break;
+    default:
+        text = lbl_8023E28C[7];
+        break;
+    }
+
+    *result = fn_801E394C(text);
+    return text;
+}
